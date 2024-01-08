c058e4d6b0b1ff9e028677b5505b7e4d1688f7f6
Anna Lebid <annalez16@gmail.com>
2024-01-08 10:45:03 +0200

    add layout and header component

---
+++ components/app-header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="flex justify-between items-center">
+    <NuxtLink to="/" class="text-xl font-bold">Finance tracker</NuxtLink>
+
+    <div>
+      <UAvatar
+        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        alt="Avatar"
+      />
+    </div>
+  </header>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
