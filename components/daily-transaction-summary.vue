789aee4afd729abe1f3db7ca337073d5e3be0fae
Anna Lebid <annalez16@gmail.com>
2024-01-11 17:29:21 +0200

    create daily-transactions component

---
+++ components/daily-transaction-summary.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="grid grid-cols-2 py-4 border-b border-gray-200 dark:border-gray-800 text-gray-500 dark:text-gray-400 font-bold"
+  >
+    <div class="flex justify-between items-center">{{ date }}</div>
+
+    <div class="flex items-center justify-end space-x-2 mr-10">
+      {{ currency }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({ date: String, transactions: Array });
+const sum = computed(() => {
+  return props.transactions.reduce(
+    (acc, current) =>
+      current.type === "Income" ? acc + current.amount : acc - current.amount,
+    0
+  );
+});
+
+const { currency } = useCurrency(sum);
+</script>
