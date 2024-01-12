ebcd36a7a68fcb8fb94f00efbb4c84cce6c8ad74
Anna Lebid <annalez16@gmail.com>
2024-01-12 21:44:47 +0200

    create form

--- components/transaction-modal.vue
+++ components/transaction-modal.vue
@@ -3,12 +3,51 @@
     <UCard>
       <template #header> Add Transaction </template>
 
-      <div>Hello!</div>
+      <UFormGroup
+        label="Transaction Type"
+        :required="true"
+        name="type"
+        class="mb-4"
+      >
+        <USelect
+          placeholder="Select the transaction type"
+          :options="transactionTypes"
+        />
+      </UFormGroup>
+      <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
+        <UInput type="number" placeholder="Amount" />
+      </UFormGroup>
+      <UFormGroup
+        label="Transaction Date"
+        :required="true"
+        name="created_at"
+        class="mb-4"
+      >
+        <UInput type="date" icon="" i-heroicons-calendar-days-20-solid />
+      </UFormGroup>
+      <UFormGroup
+        label="Description"
+        hint="Optional"
+        name="description"
+        class="mb-4"
+      >
+        <UInput type="text" placeholder="Description" />
+      </UFormGroup>
+      <UFormGroup
+        :required="true"
+        label="Category"
+        hint="Optional"
+        class="mb-4"
+      >
+        <USelect placeholder="Category" :options="categories" />
+      </UFormGroup>
+      <UButton type="submit" color="primary" variant="solid" label="Save" />
     </UCard>
   </UModal>
 </template>
 
 <script setup>
+import { categories, transactionTypes } from "~/constants";
 const props = defineProps({
   modelValue: Boolean,
 });
--- constants.ts
+++ constants.ts
@@ -1,1 +1,18 @@
 export const transactionViewOptions = ["Yearly", "Monthly", "Weekly", "Daily"];
+export const categories = [
+  "Food",
+  "Housing",
+  "Transportation",
+  "Utilities",
+  "Clothing",
+  "Medical",
+  "Insurance",
+  "Personal",
+  "Debt",
+  "Education",
+  "Savings",
+  "Gifts",
+  "Entertainment",
+  "Others",
+];
+export const transactionTypes = ["Income", "Expense", "Saving", "Investment"];
