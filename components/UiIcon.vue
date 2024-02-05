8ced9c5441889afdd8d8eb27da5a30eb61705ad4
Anna <anna@finqai.com>
2024-02-05 11:27:20 +0200

    feat(ui): create reusable Spinner and Icon component

---
+++ components/UiIcon.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { cva } from "class-variance-authority";
+import { cn } from "../lib/utils";
+
+const props = defineProps<{
+  name: string;
+  class?: string;
+  color?: "primary" | "white" | "neutralDark" | "neutralLight";
+  size?: "s" | "m" | "l" | "xl";
+}>();
+
+const iconVariants = cva("inline-flex", {
+  variants: {
+    size: {
+      s: "h-3 w-3",
+      m: "h-4 w-4",
+      l: "h-5 w-5",
+      xl: "h-6 w-6",
+    },
+    color: {
+      primary: "#222222",
+      white: "#FFFFFF",
+      neutralDark: "#808080",
+      neutralLight: "#B2B2B2",
+    },
+  },
+  defaultVariants: {
+    size: "s",
+    color: "primary",
+  },
+});
+
+const classes = computed(() => {
+  return cn(
+    iconVariants({ size: props.size, color: props.color }),
+    props.class
+  );
+});
+</script>
+
+<template>
+  <Icon name="props.name" :class="classes" />
+</template>
+
+<style lang="scss" scoped></style>
