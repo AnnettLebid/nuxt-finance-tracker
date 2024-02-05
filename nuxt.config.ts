e70b6f5aa0acaa3fb2cdf251aa2180daeeabbf32
Anna <anna@finqai.com>
2024-02-05 10:12:46 +0200

    chore(deps): add dependencies: clsx, tailwind-merge, tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/supabase"],
+  modules: ["@nuxt/ui", "@nuxtjs/supabase", "@nuxtjs/tailwindcss"],
   supabase: {
     redirect: false,
+  },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  vue: {
+    compilerOptions: {
+      isCustomElement: (tag) => ["lite-youtube"].includes(tag),
+    },
+  },
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+  devServer: {
+    port: 3001,
   },
 });
