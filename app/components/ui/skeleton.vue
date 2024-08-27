f1b0127b7d96ba4dfb3f055a9a160e09155873c2
Anna Lebid <annalez16@gmail.com>
2024-08-27 20:32:47 +0300

    feat(ui): create skeleton component

---
+++ app/components/ui/skeleton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    v-if="loading"
+    :class="cn('bg-slate-200 animate-pulse h-5 w-full rounded-md', props.class)"
+  />
+  <div v-else>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ClassValue } from "class-variance-authority/types";
+import { cn } from "../../lib/utils";
+
+const props = defineProps<{
+  class?: ClassValue;
+  loading: boolean;
+}>();
+</script>
