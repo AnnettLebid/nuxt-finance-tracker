4970723a8b2514db85452ef908b657189a3eda50
Anna Lebid <annalez16@gmail.com>
2024-09-06 01:30:38 +0300

    fix: rename transactions to Item and move to another folder

--- app/components/General/Header.vue
+++ app/components/General/Header.vue
@@ -3,8 +3,14 @@
     :class="cn('flex justify-between items-baseline', props?.classes?.root)"
   >
     <div>
-      <h3 :class="cn('font-semibold', props?.classes?.title)">{{ title }}</h3>
-      <h4 :class="cn('font-light', props?.classes?.subtitle)">
+      <h3 :class="cn('font-semibold text-lg', props?.classes?.title)">
+        {{ title }}
+      </h3>
+      <h4
+        :class="
+          cn('font-light text-black/50 text-base', props?.classes?.subtitle)
+        "
+      >
         {{ subTitle }}
       </h4>
     </div>
