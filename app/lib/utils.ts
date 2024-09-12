49b43f81e36433da49b79f2444cacdaba3c89571
Anna Lebid <annalez16@gmail.com>
2024-09-13 01:19:02 +0300

    fix: remove logs

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -2,7 +2,5 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  console.log("inputs", ...inputs);
-  console.log("clsx(inputs)", clsx(inputs));
   return twMerge(clsx(inputs));
 }
