df3321b41cb68b46d082e04d7165d7fcde729774
Anna Lebid <annalez16@gmail.com>
2024-09-16 17:53:44 +0300

    feat: login user with supabase

---
+++ app/pages/confirm.vue
@@ -0,0 +1,11 @@
+<template>
+  <UCard>
+    <template #header>
+      <p class="text-xl font-bold">Signing in ...</p>
+    </template>
+  </UCard>
+</template>
+
+<script setup lang="ts">
+useRedirectIfAuth();
+</script>
---
+++ app/pages/login.vue
@@ -0,0 +1,72 @@
+<template>
+  <UCard v-if="!success">
+    <template #header> Sign in to Finance tracker</template>
+
+    <form @submit.prevent="handleLogin">
+      <UFormGroup
+        label="email"
+        name="email"
+        class="mb-4"
+        :required="true"
+        help="You will receive email with a confirmation link"
+      >
+        <UInput type="email" placeholder="Email" required v-model="email" />
+      </UFormGroup>
+
+      <UiButton type="submit" :loading="isLoading">Sign in</UiButton>
+    </form>
+  </UCard>
+  <UCard v-else>
+    <template #header> Email has been sent</template>
+    <div class="text-center">
+      <p class="mb-4">
+        We have sent email to <strong>{{ email }}</strong
+        >with a link to sign in
+      </p>
+      <p>Important: the link will expire in 5 minutes!</p>
+    </div>
+  </UCard>
+</template>
+
+<script setup lang="ts">
+const toast = useToast();
+const supabase = useSupabaseClient();
+
+const success = ref<boolean>(false);
+const email = ref<string>("");
+const isLoading = ref<boolean>(false);
+
+useRedirectIfAuth();
+
+const handleLogin = async () => {
+  isLoading.value = true;
+  try {
+    const { data, error } = await supabase.auth.signInWithOtp({
+      email: email.value,
+      options: { emailRedirectTo: "http://localhost:3000/confirm" },
+    });
+
+    if (error) {
+      return toast.add({
+        title: "Error authenticating",
+        icon: "i-heroicons-exclamation-circle",
+        description: error.message,
+        color: "red",
+      });
+    }
+
+    success.value = true;
+  } catch (error) {
+    toast.add({
+      title: "Error authenticating",
+      icon: "i-heroicons-exclamation-circle",
+      description: error.message,
+      color: "red",
+    });
+  } finally {
+    isLoading.value = false;
+  }
+};
+</script>
+
+<style scoped></style>
